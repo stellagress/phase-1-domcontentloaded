57407c1987ee02a4ac7c2e56bd13f522bf29f176
Stella Mazzillo <stellagress5@gmail.com>
2023-04-06 16:11:11 -0400

    complete the js domcontentloaded event

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let p = document.getElementById('text')
+    p.textContent = 'This is really cool!'
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
